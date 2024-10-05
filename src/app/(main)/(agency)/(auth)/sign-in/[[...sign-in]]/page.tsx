053c35b5ebff19e33fab0f089e0076d53c956eeb
Prince Pal <princepal9120@gmail.com>
2024-10-05 18:06:36 +0530

    auth clerk setup and middleware

---
+++ src/app/(main)/(agency)/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+function page() {
+  return (
+    <div><SignIn/></div>
+  )
+}
+
+export default page    
---
+++ src/app/(main)/(agency)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+function Page() {
+  return (
+    <div><SignUp /></div>
+  )
+}
+
+export default Page
